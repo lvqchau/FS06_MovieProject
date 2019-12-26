8d18815068401f62607a1e0bcff124b3ee6dc0bc
Quỳnh Châu <lvqchau@apcs.vn>
2019-12-26 15:26:14 +0700

    seat view image

--- web/src/pages/Ticket/index.js
+++ web/src/pages/Ticket/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import Image from '../../components/Image';
 
 const styles = {
   container: {
@@ -21,7 +22,10 @@ const styles = {
   },
   movie: {
     background: 'aqua',
-    transition: 'width 0.3s ease'
+    transition: 'all 0.3s ease',
+    overflow: 'hidden',
+    display: 'flex',
+    justifyContent: 'flex-end'
   },
   viewContainer: {
     background: 'salmon',
@@ -29,11 +33,15 @@ const styles = {
   },
   seatChoice: {
     position: 'absolute',
-    background: 'darkblue',
-    transition: 'width 0.3s ease',
     height: '100%',
+    width: '30%',
     top: 0,
-    right: 0
+    right: 0,
+    background: 'darkblue',
+    transition: 'right 0.3s ease'
+  },
+  image: {
+    width: '35vw'
   }
 };
 
@@ -43,19 +51,19 @@ const Ticket = (props) => {
   const [view, setView] = useState(0)
   const [width, setWidth] = useState({
     movWidth: "35%",
-    seatWidth: "0%"
+    seatRight: "-30%"
   })
 
   useEffect(() => {
     if (view == 0)
       setWidth({
         movWidth: "35%",
-        seatWidth: "0%"
+        seatRight: "-30%"
       })
     else 
       setWidth({
         movWidth: "5%",
-        seatWidth: "30%"
+        seatRight: "0%"
       })
   },[view])
 
@@ -67,20 +75,29 @@ const Ticket = (props) => {
       </div>
       <div className={classes.flexContainer}>
         {/* contains movie image */}
-        <div className={classes.movie} style={{ width: width.movWidth }}>movie</div>
+        <div className={classes.movie} style={{ width: width.movWidth }}>
+          <Image src="https://s3img.vcdn.vn/123phim/2019/11/chi-chi-em-em-sister-sister-c18-15747394235000.jpg" className={classes.image}/>
+        </div>
         {/* contains ticket count or contains seat list */}
         {
           view == 0 ?
             <div className={classes.viewContainer}>
-              <div className={classes.ticketBox}>ticketbox</div>  
+              <div className={classes.ticketBox}>TICKETTTTTTT COUNTTTTTTT</div>  
             </div>
             :
             <div className={classes.viewContainer}>
-              <div className={classes.seatList}>seatlist</div> 
+              <div className={classes.seatList}>SEATTTTTTT LISTTTTTT</div> 
             </div>
           }
         {/* contains seat choice view */}
-        <div className={classes.seatChoice} style={{ width: width.seatWidth }}>seatchoice</div>
+        <div className={classes.seatChoice} style={{ right: width.seatRight }}>
+          <Button onClick={() => setView(0)}>Choose ticket</Button>
+          <Button onClick={() => setView(0)}>Choose ticket</Button>
+          <Button onClick={() => setView(0)}>Choose ticket</Button>
+          <Button onClick={() => setView(0)}>Choose ticket</Button>
+          <Button onClick={() => setView(0)}>Choose ticket</Button>
+          <Button onClick={() => setView(0)}>Choose ticket</Button>
+        </div>
       </div>
     </div>
   );
